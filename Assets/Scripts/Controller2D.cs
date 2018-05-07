31ec88ca24edcde5c1cc51d9fc32ee120d777c3e
RAYELOY-PC\RAYELOY <rayeloy@hotmail.com>
2018-05-07 16:46:07 +0200

    Update

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    const float skinWidth= .015f;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
+    BoxCollider2D coll;
+    RaycastOrigins raycastOrigins;
+    public CollisionInfo collisions;
+
+
+    private void Start()
+    {
+        coll = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+
+    public void Move(Vector3 vel)
+    {
+        UpdateRaycastOrigins();
+        collisions.Reset();
+
+        if (vel.x != 0)
+        {
+            HorizontalCollisions(ref vel);
+        }
+
+        if(vel.y != 0)
+        {
+            VerticalCollisions(ref vel);
+        }
+
+
+        transform.Translate(vel);
+    }
+
+    void HorizontalCollisions(ref Vector3 vel)
+    {
+        float directionX = Mathf.Sign(vel.x);
+        float rayLength = Mathf.Abs(vel.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                vel.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+
+                collisions.left = directionX == -1;
+                collisions.right = directionX == 1;
+            }
+        }
+    }
+
+    void VerticalCollisions(ref Vector3 vel)
+    {
+        float directionY = Mathf.Sign(vel.y);
+        float rayLength = Mathf.Abs(vel.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + vel.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY*rayLength, Color.red);
+
+            if (hit)
+            {
+                vel.y = (hit.distance-skinWidth) * directionY;
+                rayLength = hit.distance;
+
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+            }
+        }
+    }
+
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = coll.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = coll.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+        }
+    }
+}
