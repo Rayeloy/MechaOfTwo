464d0c5e277d28b7fa9be15196621c8ac11186f7
Eloy <rayeloy@hotmail.com>
2018-05-21 18:53:01 +0200

    Game logic, cities, enemyAI started

---
+++ Assets/Scripts/City.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour {
+
+    float cityHP = 100;
+    //-----HP BAR-----
+
+    private void Update()
+    {
+        if (cityHP <= 0)
+        {
+            DestroySelf();
+        }
+    }
+
+    public void DestroySelf()
+    {
+        GameController.instance.citiesDestroyed++;
+        //animacion particulas
+        Destroy(this.gameObject);
+    }
+
+    public void DamageCity(float damage)
+    {
+        cityHP -= damage;
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,5 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    public float hp = 10;
+    public float damage = 10;
+    public float speed = 5;
+    public float turningSpeed = 1;
 
+    bool targetSelected = false;
+    Vector2 targetPos;
+
+    private void Awake()
+    {
+  
+    }
+    private void Start()
+    {
+        SelectTarget();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void SelectTarget()
+    {
+        if (!targetSelected)
+        {
+            int r = Random.Range(0, GameController.instance.cities.Length);
+            targetPos = GameController.instance.cities[r].position;
+        }
+    }
+
+    void Move()
+    {
+
+    }
+
+    void DestroySelf()
+    {
+        //animacion particulas explosion
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.tag)
+        {
+            case "City":
+                col.GetComponent<City>().DamageCity(damage);
+                DestroySelf();
+                break;
+            case "Bullet":
+                DestroySelf();
+                break;
+        }
+    }
 }
